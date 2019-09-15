937bffc1f5478d62b806ab48a5a4ece63b4f8bb4
Brandon Surmanski <b.surmanski@gmail.com>
2019-09-15 15:20:23 -0400

    add support for attaching custom gamedata to a context

--- src/context.rs
+++ src/context.rs
@@ -1,16 +1,18 @@
 use sdl2::Sdl;
 use crate::window::Window;
 use std::time;
+use std::cell::RefCell;
 
-pub struct Context {
+pub struct Context<T> {
     sdl: Sdl,
     pub sdl_video: sdl2::VideoSubsystem,
     pub sdl_event_pump: sdl2::EventPump,
     window: Option<Window>,
     running: bool,
+    pub game_data: Option<RefCell<T>>,
 }
 
-impl Context {
+impl<T> Context<T> {
     pub fn new() -> Self {
         let sdl_context = sdl2::init().unwrap();
         let sdl_video = sdl_context.video().unwrap();
@@ -21,11 +23,12 @@ impl Context {
             sdl_event_pump: sdl_event_pump,
             window: None,
             running: true,
+            game_data: None,
         }
     }
 
     pub fn open_window(&mut self, title: String, w: usize, h: usize) -> &Window {
-        self.window = Some(Window::new(self, title, w, h));
+        self.window = Some(Window::new(&self.sdl_video, title, w, h));
         dbg!("opening window");
         return self.window();
     }
@@ -42,7 +45,15 @@ impl Context {
         self.window.as_ref().unwrap().swap_buffers();
     }
 
-    pub fn run(&mut self, tick_fn: &mut FnMut(&mut Context, time::Duration)) {
+    pub fn attach_game_data(&mut self, t: T) {
+        self.game_data = Some(RefCell::new(t));
+    }
+
+    pub fn game_data(&self) -> Option<&RefCell<T>> {
+        self.game_data.as_ref()
+    }
+
+    pub fn run(&mut self, tick_fn: &mut FnMut(&mut Context<T>, time::Duration)) {
         let mut time = time::Instant::now();
         #[cfg(target_os = "emscripten")] {
             let mut s = self;
